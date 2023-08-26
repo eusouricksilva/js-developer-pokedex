f217b18f432ce3bf10f9a58e8229dd5d3b9e2713
Pedro Henrique <pedrohenriquedasilvacontato@gmail.com>
2023-08-26 00:27:14 -0300

    Projeto Pokedex - Finalizado

---
+++ assets/js/pokemon-model.js
@@ -0,0 +1,18 @@
+
+class Pokemon {
+    number;
+    name;
+    type;
+    types = [];
+    photo;
+
+    species;
+    height;
+    weight;
+    abilities = [];
+    hp;
+    attack;
+    defense;
+    speed;
+    total;
+}
